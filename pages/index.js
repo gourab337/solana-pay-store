c0545c9d95f09580978d3a239a299c49cef0aad6
gourab337 <19bcs118@iiitdwd.ac.in>
2023-01-09 20:31:10 +0530

    initial

---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+import Product from '../components/Product';
+import CreateProduct from '../components/CreateProduct';
+
+// Constants
+const TWITTER_HANDLE = "gourab337";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji"/>
+
+      <div className='button-container'>
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className='products-container'>
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+  
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="header-container">
+          <p className="header">Film Store 📸</p>
+          <p className="sub-header">Powered by Solana Pay</p>
+          <p className="sub-text">A film store that accepts sh*tcoins</p>
+
+          {isOwner && (
+            <button className='create-product-button' onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+        </header>
+
+        <main>
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
